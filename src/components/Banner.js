29ea84e87a95b9241b5987e223a47f0590d797e9
syaifudin alkatiri <sfnalkatiri@gmail.com>
2022-10-25 14:47:03 +0700

    data fetch

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+
 const Banner = (props) => {
 	const [image, setImage] = useState("");
 	useEffect(() => {
 		axios
 			.get(
-				"https://api.unsplash.com/photos/random?client_id=fQm0v3q0FwbW8p5PH18jJ_SDx1aTNffpQRgfUfVneDU"
+			`https://api.unsplash.com/photos/random?client_id=${process.env.REACT_APP_API_KEY}`
 			)
 			.then((response) => {
-				console.log(response.data);
+				// setTimeout(() => {
+				// 	setImage(response.data.urls.full);
+				// }, 3000);
 				setImage(response.data.urls.full);
 			});
 	}, []);
+	useEffect(() => {}, [image]);
 	return (
 		<div id="banner">
-			<img src={image} alt="" className="image" />
+			{image ? (
+				<img src={image} alt="" className="image" />
+			) : (
+				<Skeleton height={30} />
+			)}
 		</div>
 	);
 };
